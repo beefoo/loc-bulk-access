1487e2e76822e2a00d865e143ba60ce1cf1c058d
beefoo <brian@youaremyjoy.org>
2023-09-19 16:34:52 -0400

    build API URL for making and validating requests

--- loc-bulk-access/ui/bulk-access.js
+++ loc-bulk-access/ui/bulk-access.js
@@ -1,19 +1,28 @@
 class BulkAccess {
   constructor(options = {}) {
-    const defaults = {};
+    const defaults = {
+      appendParamsToURL: {},
+      baseURL: 'https://www.mywebsite.org/',
+      validator: (apiResponse) => ({ valid: false, type: 'Unknown', count: 0 }),
+    };
     this.options = Object.assign(defaults, options);
     this.init();
   }
 
   init() {
-    this.$el = document.getElementById('main');
+    this.el = document.getElementById('main');
 
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       this.processURL(tabs[0].url);
     });
   }
 
-  processURL(url) {
-    this.$el.innerHTML = url;
+  async processURL(url) {
+    const { el } = this;
+    el.classList.add('is-loading');
+    const apiURL = Utilities.appendParamsToURL(url, this.options.appendParamsToURL);
+    const response = await fetch(apiURL);
+    const apiResponse = await response.json();
+    const valid = this.options.validator(apiResponse);
   }
 }
--- loc-bulk-access/ui/loc-main.js
+++ loc-bulk-access/ui/loc-main.js
@@ -1,3 +1,30 @@
+// This file should contain all logic specific to Library of Congress JSON API
+// The main bulk-access.js should be agnostic to data source and one day could support other APIs
 (function initLOC() {
-  const app = new BulkAccess({});
+  // function for validating an API response
+  const validator = (apiResponse) => {
+    const resp = { valid: false, type: 'Unknown', count: 0 };
+    if ('item' in apiResponse) {
+      resp.valid = true;
+      resp.type = 'Item';
+      resp.count = 1;
+    } else if ('pagination' in apiResponse && 'results' in apiResponse) {
+      if (apiResponse.results.length > 0 && 'item' in apiResponse.results[0]) {
+        resp.valid = true;
+        resp.type = 'Search result';
+        resp.count = apiResponse.pagination.of;
+        if ('site_type' in apiResponse && apiResponse.site_type === 'collections') {
+          resp.type = 'Collection';
+        }
+      }
+    }
+    return resp;
+  };
+
+  const config = {
+    appendParamsToURL: { fo: 'json', c: 150 },
+    baseURL: 'https://www.loc.gov/',
+    validator,
+  };
+  return new BulkAccess(config);
 }());
